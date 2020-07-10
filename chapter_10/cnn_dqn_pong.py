e74f27ed5878159cadd58338bf325d14179e2483
Youn-Hee Han <yh21.han@gmail.com>
2020-07-10 18:54:57 +0900

    ...

--- chapter_10/cnn_dqn_pong.py
+++ chapter_10/cnn_dqn_pong.py
@@ -1,3 +1,8 @@
+import sys, os
+idx = os.getcwd().index("link_rl_book_codes")
+PROJECT_HOME = os.path.join(os.getcwd()[:idx-1], "link_rl_book_codes")
+sys.path.append(PROJECT_HOME)
+
 from PIL import Image
 from gym.spaces import Box
 
